fed5e511eaacb7adc5c4f9f6f946028673828b40
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-09-13 21:52:39 -0700

    Added authenticated rating post endpiont

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ using NetTopologySuite;
 using NetTopologySuite.Geometries;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -30,6 +31,7 @@ namespace MoviesAPI
     {
         public Startup(IConfiguration configuration)
         {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
             Configuration = configuration;
         }
 
